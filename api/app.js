488b01b63689b4aee4c254e1fbfdebde56489e48
Seyfullah Sahin <seyfullahsahinozdemir@gmail.com>
2023-08-18 22:55:07 +0300

    fix cors policy and default entries

--- api/app.js
+++ api/app.js
@@ -4,9 +4,9 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+var cors = require("cors");
 var app = express();
-
+app.use(cors());
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
